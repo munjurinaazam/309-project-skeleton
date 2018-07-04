bc9bc51aac599dee5930e12152114d8b00cb43ee
Munjurina Azam <1521880@iub.edu.bd>
2018-07-04 03:31:22 +0000

    assignment1

--- config/env/all.js
+++ config/env/all.js
@@ -1,8 +1,14 @@
-'use strict';
+
+
+ 'use strict';
+
+
+
 
 module.exports = {
 	port: process.env.PORT || 3000,
 	// The secret should be set to a non-guessable string that
 	// is used to compute a session hash
+	
 	sessionSecret: 'project309'
 };
